c425bb91f8d970ff8c386159f119058e0a7307a7
unknown <jagdeeshbhavani22@gmail.com>
2023-11-19 19:05:41 +0530

    Second Commit

--- CrickInformerBackend/CrickInformerBackend/src/main/java/com/crick/apis/CrickInformerBackendApplication.java
+++ CrickInformerBackend/CrickInformerBackend/src/main/java/com/crick/apis/CrickInformerBackendApplication.java
@@ -3,13 +3,17 @@ package com.crick.apis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "entities")
+@EnableJpaRepositories(basePackages = "repositories")
+@ComponentScan(basePackages = {"controllers", "services", "repositories"})
 public class CrickInformerBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrickInformerBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrickInformerBackendApplication.class, args);
+    }
 
 }
--- CrickInformerBackend/CrickInformerBackend/src/main/java/repositories/MatchRepo.java
+++ CrickInformerBackend/CrickInformerBackend/src/main/java/repositories/MatchRepo.java
@@ -3,9 +3,10 @@ package repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import entities.Match;
-
+@Repository
 public interface MatchRepo extends JpaRepository<Match,Integer>{
 Optional<Match> findByTeamHeading(String teamHeading);
 }
